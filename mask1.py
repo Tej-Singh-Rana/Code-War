bf44e34e4b0c6cd351bb4861905bc16d605c6d65
singh <singh@localhost>
2019-11-30 01:07:24 -0500

    Given value square or not

---
+++ mask1.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+#Value of rectangle
+
+print("Enter value : ")
+length,breadth=int(input()), int(input())
+
+if length == breadth:
+    print("It is square")
+else:
+
+    print("It is not square")
+
+
