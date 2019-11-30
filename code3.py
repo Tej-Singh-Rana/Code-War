cc5beb7c2c3cfe96b163cd6a3bc8308ceba1de3b
singh <singh@localhost>
2019-11-30 00:53:07 -0500

    To reverse word

---
+++ code3.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+#reverse !!
+
+name=input("Enter the word you want to reverse : ")
+print(name[::-1],end='')     #to reverse infinite not adding value in parameters.
+print('\n')
+
+#print(name[4::-1],end='')            #to reverse in max 4 index values. 
+#print('\n')
