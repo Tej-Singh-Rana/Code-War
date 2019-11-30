739bc80f6981159517c0a7d0519efb0e17402b4f
singh <singh@localhost>
2019-11-30 01:08:39 -0500

    2 int value comparison greater then or not

---
+++ mask2.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+#taking two int value from user.
+
+print("Enter the two integer value : ")
+value,volt=int(input()),int(input())
+
+if value>volt:
+    print("First Value is greater then second!!")
+elif value == volt:
+    print("Both are equal!!")
+else:
+    print("Second value is greater then first!!")
+
+
+
+
+
+
+
+
+
+
+
